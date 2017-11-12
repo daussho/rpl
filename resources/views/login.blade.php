8f79b2876f817f82faf6eb2189e78a09945305d9
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2017-11-12 16:33:23 +0700

    .

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,6 +11,7 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+      <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
    </head>
    <body>
       <?php
