ca43b37eec0fc6148853d1cad260c9e301f90efa
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2017-11-12 16:21:27 +0700

    testing

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,11 +10,11 @@
       <title>Login page</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+      <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
    </head>
    <body>
       <?php
-         #include "navbar/home-navbar.html"
+       		@include('navbar/navbar-supervisor')
       ?>
       </br></br></br>
       <center>
