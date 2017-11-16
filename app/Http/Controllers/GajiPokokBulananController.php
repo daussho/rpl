2cd3daa4c28e5b0c987d47e78f3591e0dbd1241c
keyisteo <keyisteo.kingdom@gmail.com>
2017-11-16 18:46:39 +0700

    gaji pokok bulanan create pada controller

--- app/Http/Controllers/GajiPokokBulananController.php
+++ app/Http/Controllers/GajiPokokBulananController.php
@@ -3,34 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\GajiPokokBulanan;
+use App\Http\Controllers\Controller;
 
 class GajiPokokBulananController extends Controller
 {
-    //
+    //mengarahkan ke halaman retrieve
 	public function index(){
-		$gajipokok = DB::table('gaji_pokok_bulanan')->get();
+		$gajipokok = DB::select('SELECT id, nip,bulan,nominal FROM gaji_pokok_bulanans');
         return view('gajipokok.index', ['gajipokok' => $gajipokok]);
 	}
+	//Mengarahkan ke halaman create
 	public function create(){
-		
+	//	DB::insert('INSERT INTO gaji_pokok_bulanans (nip, bulan,nominal ) values (?, ?,?)', [$request->nip, $request->, $request->nominal]);
+		return view('gajipokokcreate');
 	}
+	//Melakukan store dari request form
 	public function store(Request $request){
 		$gajipokok = new GajiPokokBulanan;
-
-        $gajipokok-> = $request->name;
         $gajipokok->nip= $request->nip;
         $gajipokok->bulan =$request->bulan;
         $gajipokok->nominal =$request->nominal;
         $gajipokok->save();
+        return redirect('/');
 	}
-	public function edit($id){
-		
+	//mengarahkan ke halaman edit
+	public function edit(){
+
 	}
+	//melakukan update pada model
 	public function update($id){
-		
+		DB::update('UPDATE gaji_pokok_bulanans SET nominal=? WHERE nip= ? AND bulan=?', [$nominal, $nip, $bulan]);
 	}
+	//Mendelete record
 	public function destroy($id){
-		
+		$deleted = DB::delete('DELETE FROM users WHERE id=?',[$id]);
+
 	}
 
 }
