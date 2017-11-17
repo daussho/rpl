cdfae5d581311a600c37f3314f39afded4e924bf
keyisteo <keyisteo.kingdom@gmail.com>
2017-11-17 12:28:12 +0700

    Nambahin controller gaji lembur dan resourcesnya

--- app/Http/Controllers/GajiLemburController.php
+++ app/Http/Controllers/GajiLemburController.php
@@ -11,12 +11,12 @@ class GajiLemburController extends Controller
 {
 	    //mengarahkan ke halaman retrieve
 	public function index(){
-		$gajilembur = DB::select('SELECT id, nip,bulan,nominal FROM gaji_lemburs');
+		$gajipokok = DB::table('gaji_lemburs')->select('nip','bulan','gaji_lembur_jam','gaji_lembur','jam_lembur')->get();
         return view('gajilembur.index', ['gajilembur' => $gajilembur]);
 	}
 	//Mengarahkan ke halaman create
 	public function create(){
-		return view('gajilemburcreate');
+		return view('gajilembur.create');
 	}
 	//Melakukan store dari request form
 	public function store(Request $request){
@@ -25,23 +25,27 @@ class GajiLemburController extends Controller
         $gajilembur->bulan =$request->bulan;
 		$gajilembur->gaji_lembur_jam= $request->gaji_lembur_jam;
         $gajilembur->jam_lembur= $request->jam_lembur;
-        $gajilembur->gaji_lembur= $request->gaji_lembur;
+        $gajilembur->gaji_lembur= $request->jam_lembur *$request->gaji_lembur_jam;
         $gajilembur->save();
         return redirect('/');
 	}
 	//mengarahkan ke halaman edit
 	public function edit($id){
-		$gajilembur = DB::select('SELECT id, nip,bulan,nominal FROM gaji_lemburs WHERE id=?',[$id]);
-		
-		return view('gajilemburedit',["gajilembur"=>$gajilembur]);
+		$gajilembur= 
+			DB::table('gaji_pokok_bulanans')
+			->select('nip', 'bulan', 'gaji_lembur_jam','gaji_lembur','jam_lembur')
+			->where('id',$id)
+			->get();
+		return view('gajilembur.edit',["gajilembur"=>$gajilembur]);
 	}
 	//melakukan update pada model
 	public function update(Request $request){
-		DB::update('UPDATE gaji_lemburs SET nominal=? WHERE nip= ? AND bulan=?', [$request->nominal, $request->nip, $request->bulan]);
+		DB::update('UPDATE gaji_lemburs SET gaji_lembur_jam=?, jam_lembur=?, gaji_lembur=? WHERE nip= ? AND bulan=?', [$request->gaji_lembur_jam, $request->jam_lembur, $request->jam_lembur *$request->gaji_lembur_jam, $request->nip, $request->bulan]);
+		return redirect('/gajilembur');
 	}
 	//Mendelete record
 	public function destroy($id){
 		$deleted = DB::delete('DELETE FROM gaji_lemburs WHERE id=?',[$id]);
-
+		return redirect('/gajilembur');
 	}
 }
--- app/Http/Controllers/GajiPokokBulananController.php
+++ app/Http/Controllers/GajiPokokBulananController.php
@@ -11,7 +11,9 @@ class GajiPokokBulananController extends Controller
 {
     //mengarahkan ke halaman retrieve
 	public function index(){
-		$gajipokok = DB::table('gaji_pokok_bulanans')->select('nip','bulan','nominal')->get();
+		$gajipokok = DB::table('gaji_pokok_bulanans')
+			->select('nip','bulan','nominal')
+			->get();
         return view('gajipokok.index', ['gajipokok' => $gajipokok]);
 	}
 	//Mengarahkan ke halaman create
@@ -25,21 +27,26 @@ class GajiPokokBulananController extends Controller
         $gajipokok->bulan =$request->bulan;
         $gajipokok->nominal =$request->nominal;
         $gajipokok->save();
-        return redirect('/');
+        return redirect('/gajipokok');
 	}
 	//mengarahkan ke halaman edit
 	public function edit($id){
-		$gajipokok= DB::table('gaji_pokok_bulanans')->select('id', 'nip', 'bulan', 'nominal')->where('id',$id)->get();
+		$gajipokok= 
+			DB::table('gaji_pokok_bulanans')
+			->select('id', 'nip', 'bulan', 'nominal')
+			->where('id',$id)
+			->get();
 		return view('gajipokok.edit',["gajipokok"=>$gajipokok]);
 	}
 	//melakukan update pada model
 	public function update(Request $request){
 		DB::update('UPDATE gaji_pokok_bulanans SET nominal=? WHERE nip= ? AND bulan=?', [$request->nominal, $request->nip, $request->bulan]);
+		return redirect('/gajipokok');
 	}
 	//Mendelete record
 	public function destroy($id){
 		$deleted = DB::delete('DELETE FROM gaji_pokok_bulanans WHERE id=?',[$id]);
-
+		return redirect('/gajipokok');
 	}
 
 }
